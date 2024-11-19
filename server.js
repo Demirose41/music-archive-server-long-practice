82a1342bdfda43c2d80540bd0ffb26492ffb09c2
demirose41 <demirose41@gmail.com>
2024-11-18 21:56:09 -0800

    completed prompts 6 - 10

--- server.js
+++ server.js
@@ -32,6 +32,30 @@ function getNewSongId() {
   return newSongId;
 }
 
+// returns albums of a given artistId
+
+function getAlbumsByArtist(artistId){
+  const artistAlbums = []
+  for ( const [id, album] of Object.entries(albums) ){
+    if(album.artistId === Number(artistId)) {
+      artistAlbums.push(album)
+    }
+  }
+  return artistAlbums
+}
+
+// returns songs with a given albumId
+
+function getSongsByAlbum(albumId){
+  const albumSongs = [];
+  for( const [id, song] of Object.entries(songs) ){
+    if(song.albumId === Number(albumId)){
+      albumSongs.push(song)
+    }
+  }
+  return albumSongs
+}
+
 /* ======================= PROCESS SERVER REQUESTS ======================= */
 const server = http.createServer((req, res) => {
   console.log(`${req.method} ${req.url}`);
@@ -89,13 +113,7 @@ const server = http.createServer((req, res) => {
         return res.end(); 
       }
       let artist = artists[urlParts[2]];
-      let artistAlbums = []
-      for ( const [id, album] of Object.entries(albums) ){
-        if(album.artistId === Number(urlParts[2])) {
-          artistAlbums.push(album)
-        }
-      }
-      artist["albums"] = artistAlbums;
+      artist["albums"] = getAlbumsByArtist(urlParts[2]);
       res.body = JSON.stringify(artists[urlParts[2]])
       res.end(res.body)
       return;
@@ -140,11 +158,105 @@ const server = http.createServer((req, res) => {
         delete artists[artistId];
         res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
-        console.log(artists)
         res.end("Sucessfully deleted")
         return
       }
     }
+
+    //6. Get all albums of a specific artist based on artist Id
+    if(req.method === "GET" && req.url.startsWith("/artists")
+    && urlParts.length === 4){
+      const artistId = urlParts[2]
+      if(artists[artistId]){
+        const foundAlbums = getAlbumsByArtist(artistId)
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.body = foundAlbums;
+        res.end(JSON.stringify(res.body));
+        return
+      }
+    }
+
+    //7. Get a specific album's details based on albumId
+    if(req.method === "GET" && req.url.startsWith("/albums")
+    && urlParts.length === 3){
+      const albumId = urlParts[2];
+      if(albums[albumId]){
+        const foundSongs = getSongsByAlbum(albumId);
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        const album = albums[albumId];
+        album["songs"] = foundSongs;
+        res.body = album;
+        res.end(JSON.stringify(res.body));
+        return
+      }
+    }
+
+    //8. Add an album to a specific artist based on artistId
+    if(req.method === "POST" && req.url.startsWith("/artists")
+    && urlParts.length === 4){
+      const artistId = urlParts[2];
+      if(artists[artistId]){
+        const newAlbumId = getNewAlbumId();
+        const newAlbumName = req.body.name;
+        albums[newAlbumId] = {
+          "name": newAlbumName,
+          "albumId": newAlbumId,
+          "artistId": Number(artistId)
+        }
+      res.statusCode = 201;
+      res.setHeader("Content-Type", "application/json")
+      res.body = albums[newAlbumId]
+      return res.end(JSON.stringify(res.body));
+      }else{
+        res.statusCode = 400
+        res.write("artist not found")
+        return res.end(); 
+      }
+    }
+
+
+
+    //9. Edit a specified album by albumId
+    
+    if ((req.method === "PUT" || req.method === "PATCH") && req.url.startsWith("/albums/") && urlParts.length === 3) {
+      if(!req.body.name){ res.statusCode = 422; return res.end()}
+
+      const currentAlbumId = urlParts[2];
+      let currentAlbum = albums[currentAlbumId];
+      if(currentAlbum){
+
+
+        currentAlbum.name = req.body.name
+        
+        res.statusCode = 200
+        
+        res.setHeader("Content-Type", "application/json")
+        
+        res.body = currentAlbum;
+        
+        res.end(JSON.stringify(res.body))
+        return;
+      
+      }
+    }
+
+    //10. Delete a specified album by albumId
+    if (req.method === "DELETE" && req.url.startsWith("/albums/") && urlParts.length === 3){
+      const albumId = urlParts[2];
+      if(!albums[albumId]) { res.statusCode = 422; return res.end()}
+
+      delete albums[albumId];
+      res.statusCode = 200;
+      res.setHeader("Content-Type","application/json")
+      res.end("Successfully deleted")
+      return
+    }
+
+    
+
+    // Error Catch
     res.statusCode = 404;
     res.setHeader('Content-Type', 'application/json');
     res.write("Endpoint not found");
